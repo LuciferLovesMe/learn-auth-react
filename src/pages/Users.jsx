39ee32a943c6b43daa8dcaefb696ab5f02602c4e
LuciferLovesMe <abhiemsmile@gmail.com>
2024-12-30 15:38:04 +0700

    main: protect all dashboard pages

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "./Layout";
 import Userslist from "../components/Userslist";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getMe } from "../features/authSlice";
 
 const Users = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isError, user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      navigate("/");
+    }
+
+    if (user && user.role !== "admin") {
+      navigate("/dashboard");
+    }
+  }, [isError, user, navigate]);
+
   return (
     <Layout>
       <Userslist />
