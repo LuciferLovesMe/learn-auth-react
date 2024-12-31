a31f40b81cad9387ea99a01ff60baa0bf3b11417
LuciferLovesMe <abhiemsmile@gmail.com>
2024-12-31 18:01:52 +0700

    main: add feature create new user

--- src/components/FormAddUser.jsx
+++ src/components/FormAddUser.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const FormAddUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [role, setRole] = useState("admin");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  const saveUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/users", {
+        name: name,
+        email: email,
+        password: password,
+        confirmPassword: confPassword,
+        role: role,
+      });
+      navigate("/users");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.msg);
+      }
+    }
+  };
+
   return (
     <div>
       <h1 className="title">Users</h1>
@@ -9,11 +37,30 @@ const FormAddUser = () => {
       <div className="card is-shadowless">
         <div className="card-content">
           <div className="content">
-            <form>
+            <form onSubmit={saveUser}>
+              <p className="has-text-centered">{msg}</p>
               <div className="field">
                 <label className="label">Name</label>
                 <div className="control">
-                  <input type="text" placeholder="Name" className="input" />
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    className="input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
               </div>
               <div className="field">
@@ -23,6 +70,8 @@ const FormAddUser = () => {
                     type="password"
                     placeholder="******"
                     className="input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
               </div>
@@ -33,6 +82,8 @@ const FormAddUser = () => {
                     type="password"
                     placeholder="******"
                     className="input"
+                    value={confPassword}
+                    onChange={(e) => setConfPassword(e.target.value)}
                   />
                 </div>
               </div>
@@ -40,7 +91,10 @@ const FormAddUser = () => {
                 <label className="label">Role</label>
                 <div className="control">
                   <div className="select is-fullwidth">
-                    <select>
+                    <select
+                      value={role}
+                      onChange={(e) => setRole(e.target.value)}
+                    >
                       <option value="admin">Admin</option>
                       <option value="user">User</option>
                     </select>
@@ -49,7 +103,9 @@ const FormAddUser = () => {
               </div>
               <div className="field">
                 <div className="control">
-                  <button className="button is-success">Save</button>
+                  <button className="button is-success" type="submit">
+                    Save
+                  </button>
                 </div>
               </div>
             </form>
--- src/components/Userslist.jsx
+++ src/components/Userslist.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Userslist = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const response = await axios.get("http://localhost:5000/users");
+    setUsers(response.data);
+  };
+
+  const deleteUser = async (uuid) => {
+    await axios.delete(`http://localhost:5000/users/${uuid}`);
+    getUsers();
+  };
   return (
     <div>
       <h1 className="title">Users</h1>
       <h2 className="subtitle">List of Users</h2>
+
+      <Link to={"/users/add"} className="button is-primary mb-2">
+        Add
+      </Link>
 
       <table className="table is-stripped is-fullwidth">
         <thead>
@@ -17,13 +38,28 @@ const Userslist = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {users.map((user, i) => (
+            <tr key={user.uuid}>
+              <td>{i + 1}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+              <td>
+                <Link
+                  to={`/users/edit/${user.uuid}`}
+                  className="button is-small is-info"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => deleteUser(user.uuid)}
+                  className="button is-small is-danger ml-1"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
